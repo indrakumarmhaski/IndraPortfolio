406918a42be1793429c2c9c6bd66f98548def957
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-04-30 13:13:29 +0530

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,7 +72,14 @@ end
 puts "9 portfolio items created"
 
 
+3.times do |technology|
+    Technology.create!(
+        name: "Technology #{technology}",
+        portfolio_id: Portfolio.last.id
+    )
+end
 
+puts "3 technologies created"
 
 
 

46c6491975f8d6c7a7313994861fbf691279cb5e
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-05-03 08:45:57 +0530

    Added jquery and created copyright method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
     include CurrentUserConcern
     include DefaultPageContent
 
+    before_action :set_copyright
 
+    def set_copyright
+        @copyright = IndraViewTool::Renderer.copyright 'Indra Kumar Mhaski', 'All rights reserved'
+    end
+end
 
+module IndraViewTool
+    class Renderer
+        def self.copyright name, msg
+            "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+        end
+    end
 end

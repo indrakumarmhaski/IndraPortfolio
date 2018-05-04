12235efa693a683bd9f53a2d0b7c03f21b920a69
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-05-04 16:41:22 +0530

    Updated styles for master layout file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ end
 module IndraViewTool
     class Renderer
         def self.copyright name, msg
-            "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+            "&copy; #{Time.now.year} | <b> #{name}</b> #{msg}".html_safe
         end
     end
 end

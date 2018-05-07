d27f651e8e7efe719090f9fa6394249727e924d8
indrakumarmhaski <indrakumarmhaski@gmail.com>
2018-05-07 09:33:43 +0530

    Implementkaminari gem/pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,11 +2,12 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
   
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit ]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :toggle_status, :new, :create, :update, :edit ]}, site_admin: :all
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)      # for kaminari gem that partilates the data 
+
     @page_title = "My Portfolio Blog"
   end
 
